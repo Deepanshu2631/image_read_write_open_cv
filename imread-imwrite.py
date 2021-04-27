07cf50ebe7485e0e0016d57b98f1145f923c16f5
Deepanshu Kushwaha <56316738+Deep2631@users.noreply.github.com>
2021-04-27 12:11:38 +0530

    Add files via upload

---
+++ imread-imwrite.py
@@ -0,0 +1,10 @@
+import cv2
+img = cv2.imread('abc.jpg' , 1 )#''' read image and store in variabl img'''
+cv2.imshow('image', img)#''' showing image'''
+k = cv2.waitKey(5000)#''' wait for 5 sec '''
+if k == 27:#''' if Esc key will press'''
+    cv2.destroyAllWindows() #''''' close image '''
+elif k==ord('s'):
+#''' if s key will press new image save with abcd.jpg  '''
+    cv2.imwrite('abcd.jpg' , img)
+    cv2.destroyAllWindows()#destroy all windows
